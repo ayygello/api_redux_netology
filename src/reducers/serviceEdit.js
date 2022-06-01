8263627544e6508918d9be47660955bf5fdfda06
Nikolay Rzhevsky <ayyge@LAPTOP-0U39RI1M.(none)>
2022-06-01 17:36:43 +0300

    first commit

---
+++ src/reducers/serviceEdit.js
@@ -0,0 +1,52 @@
+import {
+  FETCH_EDITABLE_SERVICE_REQUEST,
+  FETCH_EDITABLE_SERVICE_FAILURE,
+  FETCH_EDITABLE_SERVICE_SUCCESS,
+  CHANGE_EDITABLE_SERVICE_FIELD,
+} from '../actions/actionTypes';
+
+const initialState = {
+  item: { name: '', price: '', content: '' },
+  loading: false,
+  error: null,
+};
+
+const serviceEditReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_EDITABLE_SERVICE_REQUEST:
+      return {
+        ...state,
+        loading: true,
+        error: null,
+      };
+    case FETCH_EDITABLE_SERVICE_FAILURE:
+      const { error } = action.payload;
+      return {
+        ...state,
+        loading: false,
+        error,
+      };
+    case FETCH_EDITABLE_SERVICE_SUCCESS:
+      const { item } = action.payload;
+      return {
+        ...state,
+        item,
+        loading: false,
+        error: null,
+      };
+    case CHANGE_EDITABLE_SERVICE_FIELD:
+      const { name, value } = action.payload;
+      const newItem = state.item;
+      return {
+        ...state,
+        item: {
+          ...newItem,
+          [name]: value,
+        },
+      };
+    default:
+      return state;
+  }
+};
+
+export default serviceEditReducer;
---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import { combineReducers, legacy_createStore } from 'redux';
+import serviceListReducer from '../reducers/serviceList';
+import serviceAddReducer from '../reducers/serviceAdd';
+import serviceEditReducer from '../reducers/serviceEdit';
+
+const reducer = combineReducers({
+  serviceList: serviceListReducer,
+  serviceAdd: serviceAddReducer,
+  serviceEdit: serviceEditReducer,
+});
+
+const store = legacy_createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+export default store;
